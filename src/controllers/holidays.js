f624f1f8cdae22055a56cf3c9b6a07e2a14dd049
Nikhil Vijayan <nkhilv@gmail.com>
2019-05-15 18:34:51 +0100

    Add futureHolidays method to holidays controller

--- src/controllers/holidays.js
+++ src/controllers/holidays.js
@@ -1,40 +1,15 @@
 const { bankHolidays } = require('../adapters');
 const { serviceError } = require('../errors');
+const helpers = require('../helpers');
 
 exports.listAllHolidays = async (req, res) => {
   const holidays = await bankHolidays.listAllHolidays();
   res.json(holidays);
 };
 
-exports.test = (req, res) => {
-  res.json('Hello World');
+exports.futureHolidays = async (req, res) => {
+  const response = await bankHolidays.listAllHolidays();
+  const holidays = response['england-and-wales'].events;
+  const holidaysInTheFuture = helpers.holidaysInTheFuture(holidays);
+  res.json(holidaysInTheFuture);
 };
-
-
-// module.exports = {
-//   list
-// };
-
-// const rp = require('request-promise');
-
-// let holidaysInEnglandAndWales;
-
-// exports.getData = async (req, res) => {
-//   const response = await rp({
-//     url: 'https://www.gov.uk/bank-holidays.json',
-//     method: 'GET',
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     json: true,
-//     resolveWithFullResponse: true
-//   });
-//   holidaysInEnglandAndWales = response.body['england-and-wales'].events;
-//   return holidaysInEnglandAndWales;
-// };
-
-// exports.futureHolidays = async (req, res) => {
-//   const holidaysInEnglandAndWales = await this.getData();
-//   console.log(holidaysInEnglandAndWales);
-//   res.json(holidaysInEnglandAndWales);
-// };
